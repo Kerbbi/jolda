7f0a4cda527673a6b68e5273d51ff70c02d64fce
Kerbbi <67070923+Kerbbi@users.noreply.github.com>
2023-06-06 01:12:30 +0600

    Add files via upload

---
+++ 321/js/custom.js
@@ -0,0 +1,192 @@
+
+$(document).ready(function() {
+
+    "use strict";
+    $(window).on('load', function() {
+
+      
+
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: false,
+            live: true
+        });
+        wow.init();
+    });
+
+    
+    jQuery("#load").fadeOut();
+    jQuery("#loading").delay(0).fadeOut("slow");
+
+
+   
+    $('.owl-carousel').each(function() {
+        var $carousel = $(this);
+        $carousel.owlCarousel({
+            items: $carousel.data("items"),
+            loop: $carousel.data("loop"),
+            margin: $carousel.data("margin"),
+            nav: $carousel.data("nav"),
+            dots: $carousel.data("dots"),
+            autoplay: $carousel.data("autoplay"),
+            autoplayTimeout: $carousel.data("autoplay-timeout"),
+            navText: ['<i class="fa fa-angle-left fa-2x"></i>', '<i class="fa fa-angle-right fa-2x"></i>'],
+            responsiveClass: true,
+            responsive: {
+                // breakpoint from 0 up
+                0: {
+                    items: $carousel.data("items-mobile-sm")
+                },
+                // breakpoint from 480 up
+                480: {
+                    items: $carousel.data("items-mobile")
+                },
+                // breakpoint from 786 up
+                786: {
+                    items: $carousel.data("items-tab")
+                },
+                // breakpoint from 1023 up
+                1023: {
+                    items: $carousel.data("items-laptop")
+                },
+                1199: {
+                    items: $carousel.data("items")
+                }
+            }
+        });
+    });
+
+    
+
+    $('#iq-countdown').countdown({
+        date: '10/01/2019 23:59:59',
+        offset: -8,
+        day: 'Day',
+        days: 'Days'
+    });
+
+   
+    $('#back-to-top').fadeOut();
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 250) {
+            $('#back-to-top').fadeIn(1400);
+        } else {
+            $('#back-to-top').fadeOut(400);
+        }
+    });
+    // scroll body to 0px on click
+    $('#top').on('click', function() {
+        $('top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    
+
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a.popup-img',
+        tLoading: 'Loading image #%curr%...',
+        type: 'image',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            navigateByImgClick: true,
+            enabled: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        type: 'iframe',
+        disableOn: 700,
+        mainClass: 'mfp-fade',
+        preloader: false,
+        removalDelay: 160,
+        fixedContentPos: false
+    });
+
+    $('.iq-video').magnificPopup({
+        type: 'iframe'
+    });
+
+   
+
+    $('.multiple-items').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1367,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 662,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+           
+        ],
+        asNavFor: '.static'
+    });
+    $(".single-item").slick({
+        asNavFor: '.static'
+    });
+
+
+
+
+    $('.jarallax').jarallax({
+        speed: 0.2
+    });
+
+
+
+
+});
